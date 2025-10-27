8b9e1d2c32ad40e562a0e90825ae2b67a4e5d57f
Abhishek Jaiswal <utsavlook01@gmail.com>
2025-10-27 05:26:18 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-
-
-const withPWA = require('next-pwa');
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  webpack: (config, { isServer }) => {
-    // This is to fix a bug with genkit where it uses an old version of handlebars
-    // that is not compatible with webpack.
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'handlebars': 'handlebars/dist/handlebars.js',
-    }
-    return config
-  },
   experimental: {
     serverActions: {
       maxDuration: 120, // Increase timeout to 2 minutes for AI operations
@@ -61,14 +49,4 @@ const nextConfig = {
   },
 };
 
-const pwaConfig = {
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-};
-
-// Correctly merge the PWA config with the main Next.js config
-const finalConfig = withPWA(pwaConfig)(nextConfig);
-
-module.exports = finalConfig;
+module.exports = nextConfig;
